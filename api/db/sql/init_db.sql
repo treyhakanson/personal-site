7df59f2c7066871a39b5437b5057aad7f39f0d43
Trey Hakanson <treyhakanson@gmail.com>
2018-01-14 17:16:23 -0500

    Added "recommendations" table

--- api/db/sql/init_db.sql
+++ api/db/sql/init_db.sql
@@ -1,27 +1,35 @@
 CREATE DATABASE personal_site;
 
 CREATE TABLE contact_form (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(60) NOT NULL,
-        email VARCHAR(155) NOT NULL,
-        message VARCHAR(500) NOT NULL
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(60) NOT NULL,
+   email VARCHAR(155) NOT NULL,
+   message VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE blog_posts (
-        id SERIAL PRIMARY KEY,
-        tm TIMESTAMP NOT NULL,
-        title VARCHAR(60) UNIQUE NOT NULL,
-        hook VARCHAR(155) NOT NULL,
-        banner_img VARCHAR(60) NOT NULL,
-        author_name VARCHAR(60) NOT NULL,
-        author_img VARCHAR(1000) NOT NULL,
-        blog_body JSON NOT NULL
+   id SERIAL PRIMARY KEY,
+   tm TIMESTAMP NOT NULL DEFAULT date_trunc('second', now()),
+   title VARCHAR(60) UNIQUE NOT NULL,
+   hook VARCHAR(155) NOT NULL,
+   banner_img VARCHAR(60) NOT NULL,
+   author_name VARCHAR(60) NOT NULL,
+   author_img VARCHAR(1000) NOT NULL,
+   blog_body JSON NOT NULL
 );
 
 CREATE TABLE projects (
-        id SERIAL PRIMARY KEY,
-        tm TIMESTAMP NOT NULL,
-        title VARCHAR(60) UNIQUE NOT NULL,
-        banner_img VARCHAR(60) NOT NULL,
-        project_body JSON NOT NULL
+   id SERIAL PRIMARY KEY,
+   tm TIMESTAMP NOT NULL,
+   title VARCHAR(60) UNIQUE NOT NULL,
+   banner_img VARCHAR(60) NOT NULL,
+   project_body JSON NOT NULL
+);
+
+CREATE TABLE recommendations (
+   id SERIAL PRIMARY KEY,
+   tm TIMESTAMP NOT NULL DEFAULT date_trunc('second', now()),
+   title VARCHAR(155) NOT NULL,
+   link VARCHAR(1000) NOT NULL,
+   thumbnail VARCHAR(1000) NOT NULL
 );
